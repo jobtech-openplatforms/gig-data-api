86535907c910b458d01631de47465f326815a68a
Björn Milton <bjorn@roombler.com>
2020-03-09 15:23:53 +0100

    Swithcing to ctor way of creating the cert.

--- src/Jobtech.OpenPlatforms.GigDataApi.Common.RavenDB/DocumentStoreHolder.cs
+++ src/Jobtech.OpenPlatforms.GigDataApi.Common.RavenDB/DocumentStoreHolder.cs
@@ -74,8 +74,7 @@ namespace Jobtech.OpenPlatforms.GigDataApi.Common.RavenDB
         private static X509Certificate2 GetCert()
         {
             var bytes = System.IO.File.ReadAllBytes($"/app/certs/tls.crt");
-            var cert = new X509Certificate2();
-            cert.Import(bytes);
+            var cert = new X509Certificate2(bytes);
 
             return cert;
         }
