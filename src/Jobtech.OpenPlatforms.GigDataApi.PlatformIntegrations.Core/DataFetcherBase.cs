6cd79f0a74520fb5b502c26201b0ec65117a5556
Björn Milton <bjorn@roombler.com>
2020-05-08 09:01:58 +0200

    Added some more logging.

--- src/Jobtech.OpenPlatforms.GigDataApi.PlatformIntegrations.Core/DataFetcherBase.cs
+++ src/Jobtech.OpenPlatforms.GigDataApi.PlatformIntegrations.Core/DataFetcherBase.cs
@@ -5,6 +5,7 @@ using Jobtech.OpenPlatforms.GigDataApi.Common;
 using Jobtech.OpenPlatforms.GigDataApi.Core.Entities;
 using Jobtech.OpenPlatforms.GigDataApi.PlatformIntegrations.Core.Messages;
 using Jobtech.OpenPlatforms.GigDataApi.PlatformIntegrations.Core.Models;
+using Microsoft.Extensions.Logging;
 using Rebus.Bus;
 
 namespace Jobtech.OpenPlatforms.GigDataApi.PlatformIntegrations.Core
@@ -13,10 +14,12 @@ namespace Jobtech.OpenPlatforms.GigDataApi.PlatformIntegrations.Core
         where TConnectionInfo : IPlatformConnectionInfo
     {
         private readonly IBus _bus;
+        private readonly ILogger<DataFetcherBase<TConnectionInfo>> _logger;
 
-        protected DataFetcherBase(IBus bus)
+        protected DataFetcherBase(IBus bus, ILogger<DataFetcherBase<TConnectionInfo>> logger)
         {
             _bus = bus;
+            _logger = logger;
         }
 
         public Task<TConnectionInfo> StartDataFetch(string userId, string platformId, TConnectionInfo connectionInfo,
@@ -34,6 +37,7 @@ namespace Jobtech.OpenPlatforms.GigDataApi.PlatformIntegrations.Core
         protected async Task CompleteDataFetchWithConnectionRemoved(string userId, string platformId, PlatformConnectionDeleteReason deleteReason,
             CancellationToken cancellationToken = default)
         {
+            _logger.LogInformation("Will send PlatformConnectionRemovedMessage with delete reason {DeleteReason}.", deleteReason);
             await _bus.SendLocal(new PlatformConnectionRemovedMessage(userId, platformId, deleteReason));
         }
     }
